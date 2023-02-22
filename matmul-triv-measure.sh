f122268b305e24848d5578262a27cf893ef3bd35
jiriklepl <jiriklepl@seznam.cz>
2023-02-22 12:32:33 +0100

    add matmul triv measure

--- matmul-triv-measure.sh
+++ matmul-triv-measure.sh
@@ -10,24 +10,18 @@ echo "running compilation:" 1>&2
 
 while read -r compiler; do
 	while read -r version; do
-		for block_order in $(seq 0 5); do
-			for dim_order in $(seq 0 1); do
-				output="build/matmul/$compiler/cpu-poly_${block_order}_${dim_order}-$version"
-				source="matmul/cpu-poly-$version.cpp"
+        output="build/matmul/$compiler/cpu-triv-$version"
+        source="matmul/cpu-triv-$version.cpp"
 
-				call="$compiler -o $output \
+        call="$compiler -o $output \
 $CXX_OPTIONS \
 -DNDEBUG -DNLOGGING \
 $source \
--DA_ROW -DB_ROW -DC_ROW \
--DBLOCK_I -DBLOCK_J -DBLOCK_K \
--DBLOCK_SIZE=16 -DBLOCK_ORDER=$block_order -DDIM_ORDER=$dim_order"
-				if [ "$source" -nt "$output" ]; then
-					echo "$call"
-					$call
-				fi
-			done
-		done
+-DA_ROW -DB_ROW -DC_ROW"
+        if [ "$source" -nt "$output" ]; then
+            echo "$call"
+            $call
+        fi
 	done <<EOF
 noarr
 noarr-bag
@@ -50,22 +44,22 @@ OUTPUT2="/tmp/matmul-$(uuidgen)"
 while read -r input size; do
 	echo "$(date +%H:%M:%S:) running validation on '$input' (size: $size):" 1>&2
 
-	if [ -f "$input" ]; then
-		find build/matmul -name "*cpu-poly_${block_order}_${dim_order}-*" -mindepth 2 | shuf | while read -r file; do
-			echo "$(date +%H:%M:%S:)" "$file" "$input" "$size" 1>&2
-			"$file" "$input" "$size" > "$OUTPUT"
+    if [ -f "$input" ]; then
+        find build/matmul -name "*cpu-triv-*" -mindepth 2 | shuf | while read -r file; do
+            echo "$(date +%H:%M:%S:)" "$file" "$input" "$size" 1>&2
+            "$file" "$input" "$size" > "$OUTPUT"
 
-			if [ -f "$OUTPUT2" ]; then
-				diff -u "$OUTPUT" "$OUTPUT2" || exit 1
-			fi
+            if [ -f "$OUTPUT2" ]; then
+                diff -u "$OUTPUT" "$OUTPUT2" || exit 1
+            fi
 
-			mv "$OUTPUT" "$OUTPUT2"
-		done || exit 1
+            mv "$OUTPUT" "$OUTPUT2"
+        done || exit 1
 
-		rm "$OUTPUT2"
-	else
-		echo "warning: requesting validation on nonexistent input" 1>&2
-	fi
+        rm "$OUTPUT2"
+    else
+        echo "warning: requesting validation on nonexistent input" 1>&2
+    fi
 done <<EOF
 build/matmul/matrices_64 64
 EOF
@@ -86,7 +80,7 @@ while read -r input size; do
 
 	if [ -f "$input" ]; then
 		for _ in $(seq "$REPS"); do
-			find build/matmul -name "*cpu-poly_[0-9]_[0-9]-*" -mindepth 2 | shuf | while read -r file; do
+			find build/matmul -name "*cpu-triv-*" -mindepth 2 | shuf | while read -r file; do
 				printf "%s" "$file,$input,$size,$(uname -n),$(date)," >> "$OUTPUT"
 				"$file" "$input" "$size" > /dev/null 2>> "$OUTPUT" || exit 1
 			done || exit 1
