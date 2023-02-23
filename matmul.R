ad713815f524d97b775a89cba436dcaf5ca3cfb7
jiriklepl <jiriklepl@seznam.cz>
2023-02-23 13:34:03 +0100

    improve R scripts

--- matmul.R
+++ matmul.R
@@ -1,28 +1,42 @@
 #!/usr/bin/env Rscript
 
-library('tidyverse')
+library("tidyverse")
 
-files <- list.files(path='out',pattern='matmul*')
+files <- list.files(path = "out", pattern = "matmul*")
 
-data <- lapply(files, function(x) read.csv(paste('out/', x, sep=''),header=FALSE, col.names=c('bin', 'input', 'size', 'machine', 'date', 'time')))
-data <- do.call('rbind', data)
+data <- lapply(
+    files,
+    function(x) {
+        read.csv(
+            paste("out/", x, sep = ""),
+            header = FALSE,
+            col.names = c("bin", "input", "size", "machine", "date", "time"))
+    })
+data <- do.call("rbind", data)
 
-data <- data %>% mutate(time=apply(data['time'], 1, function(x) as.numeric(str_remove(x, ' .*'))))
-data <- data %>% mutate(bin=apply(data['bin'], 1, function(x) str_replace(x, '(poly)-(.*)2', '\\12-\\2')))
-data <- data %>% mutate(kind=apply(data['bin'], 1, function(x) str_match(str_match(x, '[^/]*/[^/]*$'), '[^-]*-[^-]*')))
-data <- data %>% mutate(subkind=apply(data['bin'], 1, function(x) str_remove(str_match(x, '[^/]*/[^/]*$'), '^[^-]*-[^-]*-')))
+data <- data %>%
+    filter(size == 512)
+
+data <- data %>%
+    mutate(bin = str_replace(bin, "(poly)-(.*)2", "\\12-\\2")) %>%
+    mutate(
+        time = as.numeric(time) / 1e9,
+        kind = str_match(str_match(bin, "[^/]*/[^/]*$"), "[^-]*-[^-]*"),
+        subkind = str_remove(str_match(bin, "[^/]*/[^/]*$"), "^[^-]*-[^-]*-"))
 
 machines <- unique(data$machine)
 
 for (i in machines) {
-    local <- data %>% filter(machine==i) %>% group_by(bin, kind, size)
-    
-    local %>%
-        summarize(time=mean(time) / mean(size^3), subkind=unique(subkind)) %>%
-        ggplot(aes(x=log2(size), y=time, fill=subkind)) +
-            geom_bar(stat='identity', position='dodge') +
-            facet_grid(. ~ kind) +
-            theme(legend.position='bottom')
+    local <- data %>% filter(machine == i) %>% group_by(bin, kind, size)
 
-    ggsave(paste('matmul-', i, '-plots.pdf', sep=''))
+    local %>%
+        reframe(subkind = unique(subkind), time) %>%
+        mutate(time = time / mean(size ^ 3)) %>%
+        ggplot(aes(x = kind, y = time, fill = subkind)) +
+            geom_boxplot(position = "dodge", outlier.shape = NA) +
+            ylab("runtime per N^3 [ns]") +
+            ylim(0, max(local$time / mean(local$size ^ 3))) +
+            theme(legend.position = "bottom")
+
+    ggsave(paste("matmul-", i, "-plots.pdf", sep = ""))
 }
