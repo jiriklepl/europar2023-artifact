c9cfec9414d02fb6fe0225b291d5f88737dc6659
jiriklepl <jiriklepl@seznam.cz>
2023-02-23 13:31:21 +0100

    fix a gcc optimization misstep

--- matmul/cpu-poly-policy.cpp
+++ matmul/cpu-poly-policy.cpp
@@ -33,24 +33,24 @@ constexpr auto kernel_matmul(JStep j_step, A a, B b, C c) {
 template<class ISize, class JSize, class KSize, class A, class B, class C>
 void matmul(ISize i_size, JSize j_size, KSize k_size, A a, B b, C c) {
 #ifdef BLOCK_I
-#define I_LOOP for(std::size_t I = 0; I < i_size / std::integral_constant<std::size_t, BLOCK_SIZE>(); I++)
-#define I_STEP std::integral_constant<std::size_t, BLOCK_SIZE>()
+#define I_LOOP for(std::size_t I = 0; I < i_size / BLOCK_SIZE; I++)
+#define I_STEP ((std::size_t)BLOCK_SIZE)
 #else
 #define I_LOOP if constexpr(const std::size_t I = 0; true)
 #define I_STEP i_size
 #endif
 
 #ifdef BLOCK_J
-#define J_LOOP for(std::size_t J = 0; J < j_size / std::integral_constant<std::size_t, BLOCK_SIZE>(); J++)
-#define J_STEP std::integral_constant<std::size_t, BLOCK_SIZE>()
+#define J_LOOP for(std::size_t J = 0; J < j_size / BLOCK_SIZE; J++)
+#define J_STEP ((std::size_t)BLOCK_SIZE)
 #else
 #define J_LOOP if constexpr(const std::size_t J = 0; true)
 #define J_STEP j_size
 #endif
 
 #ifdef BLOCK_K
-#define K_LOOP for(std::size_t K = 0; K < k_size / std::integral_constant<std::size_t, BLOCK_SIZE>(); K++)
-#define K_STEP std::integral_constant<std::size_t, BLOCK_SIZE>()
+#define K_LOOP for(std::size_t K = 0; K < k_size / BLOCK_SIZE; K++)
+#define K_STEP ((std::size_t)BLOCK_SIZE)
 #else
 #define K_LOOP if constexpr(const std::size_t K = 0; true)
 #define K_STEP k_size
