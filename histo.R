9000e87472edacac94d82ddf3e8349d734b48683
jiriklepl <jiriklepl@seznam.cz>
2023-03-01 16:52:10 +0100

    better scripts

--- histo.R
+++ histo.R
@@ -15,39 +15,56 @@ data <- lapply(
 data <- do.call("rbind", data)
 
 data <- data %>%
+    filter(size >= 2 ** 19) %>%
+    filter(size < 2 ** 31) %>%
     mutate(
         time = as.numeric(time),
-        algorithm = as.vector(str_match(bin, "[^/]*(?=/[^/]*$)")),
+        implementation = as.vector(str_match(bin, "[^/]*(?=/[^/]*$)")),
         compiler = as.vector(str_match(bin, "[^/]*(?=/[^/]*/[^/]*$)")),
-        implementation = as.vector(str_match(bin, "[^/]*$")))
+        version = as.vector(str_match(bin, "[^/]*$")))
 
 for (m in unique(data$machine)) {
     on_machine <- data %>% filter(as.vector(machine == m))
     for (c in unique(on_machine$compiler)) {
-        local <- on_machine %>% filter(as.vector(compiler == c))
+        local <- on_machine %>%
+            filter(
+                as.vector(compiler == c),
+                implementation != 'cpu-range')
 
         local <- local %>%
-            group_by(algorithm, size) %>%
-            reframe(time = time / median(time), implementation, bin) %>%
-            group_by(bin, algorithm, size) %>%
+            group_by(implementation, size) %>%
+            reframe(time = time / median(time), version, bin) %>%
+            group_by(bin, implementation, size) %>%
             reframe(
                 relative.time = time,
-                implementation = unique(implementation)) %>%
+                version = unique(version),
+                ) %>%
             mutate(x = paste0(2, "^", log2(size)))
 
-        plot <- ggplot(local, aes(x = x, y = relative.time, fill = implementation)) +
-            geom_boxplot(position = "dodge") +
+        plot <-
+            ggplot(
+                local,
+                aes(x = x, y = relative.time, fill = version)) +
+            geom_boxplot(position = "dodge", outlier.shape = NA) +
             geom_hline(yintercept = 1) +
-            facet_grid(. ~ algorithm) +
+            facet_grid(. ~ implementation) +
             xlab("input text size") +
             ylab("relative runtime") +
             theme(
                 axis.text.x = element_text(angle = 45, hjust = 1),
                 legend.position = "bottom")
 
+        dist <- .1
+
+        plot <- plot + coord_cartesian(ylim = c(1 - dist, 1 + dist))
+
         if (!dir.exists("plots/histo/"))
             dir.create("plots/histo/", recursive = TRUE)
 
-        ggsave(paste0("plots/histo/", m, "-", c, ".pdf"), plot)
+        ggsave(
+            paste0("plots/histo/", m, "-", c, ".pdf"),
+            plot,
+            width = 4,
+            height = 3)
     }
 }
